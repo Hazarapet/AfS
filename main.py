fc09071535a612c572f1d35c2b6ca935bcd6b245
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-05-02 04:01:48 +0400

    main model

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
+import cv2
 import time
 import json
-import panda as pn
+import pandas as pd
 import numpy as np
-from keras.optimizers import SGD, Adam
 from utils.common import f2_score
+from keras.optimizers import SGD, Adam
+from models.A.model import model as A_model
 
 st_time = time.time()
 N_EPOCH = 5
 BATCH_SIZE = 80
+IMAGE_WIDTH = 128
+IMAGE_HEIGH = 128
 
+x_train = []
+y_train = []
 
-print("{:.2f}m Runtime".format((time.time() - st_time) / 60))
+df_train = pd.read_csv('train.csv')
+
+flatten = lambda l: [item for sublist in l for item in sublist]
+labels = list(set(flatten([l.split(' ') for l in df_train['tags'].values])))
+
+label_map = {l: i for i, l in enumerate(labels)}
+inv_label_map = {i: l for l, i in label_map.items()}
+
+print labels
+
+# for f, tags in df_train.values[:20000]:
+#     img = cv2.imread('../input/train-jpg/{}.jpg'.format(f))
+#     targets = np.zeros(17)
+#     for t in tags.split(' '):
+#         targets[label_map[t]] = 1
+#     x_train.append(cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGH)))
+#     y_train.append(targets)
+
+[model, structure] = A_model()
+
+adam = Adam(lr=0.0001, decay=0.)
+
+model.compile(loss='binary_crossentropy',
+              optimizer=adam,
+              metrics=['accuracy'])
+
+print("\n{:.2f}m Runtime".format((time.time() - st_time) / 60))
 print "====== End ======"
---
+++ models/A/model.py
@@ -0,0 +1,61 @@
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout, Activation
+from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D, AveragePooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.regularizers import l2
+
+def model(weights_path=None):
+    _model = Sequential()
+    _model.add(ZeroPadding2D((1, 1), input_shape=(3, 128, 128)))
+    _model.add(Conv2D(64, 3, 3))
+    _model.add(BatchNormalization())
+    _model.add(Activation('relu'))
+
+    _model.add(ZeroPadding2D((1, 1)))
+    _model.add(Conv2D(64, 3, 3))
+    # model.add(BatchNormalization())
+    _model.add(Activation('relu'))
+
+    _model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    _model.add(ZeroPadding2D((1, 1)))
+    _model.add(Conv2D(128, 3, 3))
+    # model.add(BatchNormalization())
+    _model.add(Activation('relu'))
+
+    _model.add(ZeroPadding2D((1, 1)))
+    _model.add(Conv2D(128, 3, 3))
+    # model.add(BatchNormalization())
+    _model.add(Activation('relu'))
+
+    _model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    _model.add(ZeroPadding2D((1, 1)))
+    _model.add(Conv2D(128, 3, 3))
+    # model.add(BatchNormalization())
+    _model.add(Activation('relu'))
+
+    _model.add(ZeroPadding2D((1, 1)))
+    _model.add(Conv2D(128, 3, 3))
+    # model.add(BatchNormalization())
+    _model.add(Activation('relu'))
+
+    _model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    # Dense layers
+    _model.add(Flatten())
+
+    _model.add(Dense(256, kernel_regularizer=l2(1e-4)))
+    _model.add(Activation('relu'))
+    _model.add(Dropout(0.5))
+
+    _model.add(Dense(256, kernel_regularizer=l2(1e-4)))
+    _model.add(Activation('relu'))
+    _model.add(Dropout(0.5))
+
+    _model.add(Dense(17, activation='sigmoid'))
+
+    if weights_path:
+        _model.load_weights(weights_path)
+
+    return [_model, 'models/A/structures/']
