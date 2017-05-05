aca4b89dc5d995f495b537a4f4f61cb7b69d7c32
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-05-05 21:52:34 +0400

    test fix

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ BATCH_SIZE = 300
 IMAGE_WIDTH = 128
 IMAGE_HEIGH = 128
 
-weights_path = 'models/A/structures/tr_l:0.058-tr_a:0.983-val_l:0.108-val_a:0.962-time:03-05-2017-21:33:39-dur:283.181.h5'
-model_structure = 'models/A/structures/tr_l:0.058-tr_a:0.983-val_l:0.108-val_a:0.962-time:03-05-2017-21:33:39-dur:283.181.json'
+weights_path = 'models/A/structures/tr_l:0.095-tr_a:0.65-val_l:0.135-val_a:0.35-time:05-05-2017-17:24:19-dur:254.928.h5'
+model_structure = 'models/A/structures/tr_l:0.095-tr_a:0.65-val_l:0.135-val_a:0.35-time:05-05-2017-17:24:19-dur:254.928.json'
 
 with open(model_structure, 'r') as model_json:
     model = model_from_json(json.loads(model_json.read()))
@@ -69,7 +69,7 @@ df_test.columns = ['image_name', 'tags']
 
 tags = []
 for r in result:
-    r = list(r > threz)
+    r = list(r > .2)
     t = [inv_label_map[i] for i, j in enumerate(r) if j]
     tags.append(' '.join(t))
 
