c964037f02a7ec7ea61850190b6a4c5ede6cb0ff
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-05-12 03:23:24 +0400

    some gdal experiments

--- exp2.py
+++ exp2.py
@@ -2,5 +2,5 @@ import json
 import cv2
 from PIL import Image
 
-img = Image.open('resource/train-augmented-jpg/{}.jpg'.format('h_fliptrain_30150'))
+img = Image.open('resource/train-jpg/{}.jpg'.format('train_30150'))
 img.show()
--- gdal_test.py
+++ gdal_test.py
@@ -1,45 +1,45 @@
 import gdal
 import numpy as np
+from PIL import Image
 from gdalconst import *
 import matplotlib.pyplot as plt
 from utils import image as image_util
 
-tif_sample = 'resource/train-tif-sample/train_10087.tif'
+tif_sample = 'resource/train-tif-sample/train_10008.tif'
+jpg_sample = 'resource/train-jpg-sample/train_10008.jpg'
 
-def read_gdalImg(path):
+def read_tif(path):
     data = gdal.Open(path)
-    rband = data.GetRasterBand(1)
+    rband = data.GetRasterBand(3)
     gband = data.GetRasterBand(2)
-    bband = data.GetRasterBand(3)
+    bband = data.GetRasterBand(1)
     nirband = data.GetRasterBand(4)
-    red = rband.ReadAsArray().astype(np.uint8)
-    green = gband.ReadAsArray().astype(np.uint8)
-    blue = bband.ReadAsArray().astype(np.uint8)
-    nir = nirband.ReadAsArray().astype(np.uint8)
+
+    red = rband.ReadAsArray().astype(np.float32)
+    green = gband.ReadAsArray().astype(np.float32)
+    blue = bband.ReadAsArray().astype(np.float32)
+    nir = nirband.ReadAsArray().astype(np.float32)
+
     return red, green, blue, nir
 
 if __name__ == '__main__':
-    data = gdal.Open(tif_sample)
-    rband = data.GetRasterBand(1)
-    gband = data.GetRasterBand(2)
-    bband = data.GetRasterBand(3)
-    nirband = data.GetRasterBand(4)
-    
-    red = rband.ReadAsArray()
-    green = gband.ReadAsArray()
-    blue = bband.ReadAsArray()
-    nir = nirband.ReadAsArray()
+    red, green, blue, nir = read_tif(tif_sample)
+
+    jpg = Image.open(jpg_sample)
 
     rgb = np.array([red, green, blue])
     rgb = rgb.transpose((1, 2, 0))
     mag, angle = image_util.img_sobel(green)
+
     ndvi = (nir - red)/(nir + red)
 
-    print np.max(green)
+    print np.max(red), np.max(ndvi)
 
-    plt.figure('green')
-    plt.imshow(green, cmap='gray')
-    plt.figure('blooming')
-    plt.imshow(mag > 500, cmap="gray")
+    plt.figure('jpg')
+    plt.imshow(jpg)
+    # plt.figure('blooming')
+    # plt.imshow(mag, cmap="gray")
+    plt.figure('ndvi')
+    plt.imshow(ndvi, cmap='gray')
     plt.show()
 
