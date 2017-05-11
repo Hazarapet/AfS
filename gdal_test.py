a374e71856f9dc8a8f41333fc299a50e85d80dfa
Hazarapet <hazarapettunanyan@gmail.com>
2017-05-11 20:25:30 +0400

    good way to find blooming

--- gdal_test.py
+++ gdal_test.py
@@ -1,8 +1,10 @@
 import gdal
 import numpy as np
 from gdalconst import *
+import matplotlib.pyplot as plt
+from utils import image as image_util
 
-tif_sample = 'resource/train-tif-sample/train_0.tif'
+tif_sample = 'resource/train-tif-sample/train_10087.tif'
 
 def read_gdalImg(path):
     data = gdal.Open(path)
@@ -19,5 +21,24 @@ def read_gdalImg(path):
 if __name__ == '__main__':
     data = gdal.Open(tif_sample)
     rband = data.GetRasterBand(1)
+    gband = data.GetRasterBand(2)
+    bband = data.GetRasterBand(3)
+    nirband = data.GetRasterBand(4)
     red = rband.ReadAsArray()
-    print red
+    green = gband.ReadAsArray()
+    blue = bband.ReadAsArray()
+    nir = nirband.ReadAsArray()
+
+    rgb = np.array([red, green, blue])
+    rgb = rgb.transpose((1, 2, 0))
+    mag, angle = image_util.img_sobel(green)
+    ndvi = (nir - red)/(nir + red)
+
+    print np.max(green)
+
+    plt.figure('green')
+    plt.imshow(green, cmap='gray')
+    plt.figure('blooming')
+    plt.imshow(mag > 500, cmap="gray")
+    plt.show()
+
