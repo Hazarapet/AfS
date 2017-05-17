995a82a14e74a39b486434296cf6b6a4f2f54308
Hazarapet <hazarapettunanyan@gmail.com>
2017-05-17 20:34:58 +0400

    fbeta

--- exp2.py
+++ exp2.py
@@ -1,11 +1,27 @@
-import json
 import cv2
-from PIL import Image
+import json
 import numpy as np
+from PIL import Image
+import keras.backend as K
+import utils.common as common
 
-A = np.arange(27).reshape((3, 3, 3))
-img = Image.fromarray(A, "RGB" )
+y_true = [[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0],
+     [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+     [1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
+     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]]
 
-print A
-print np.flip(A, 2)
+# Note that with f2 score, your predictions need to be binary
+# If you have probabilities, you will need to do some form of thresholding beforehand
+y_pred = [[0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0],
+     [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+     [1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+     [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+     [0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]]
 
+y_true, y_pred, = np.array(y_true).astype(np.float16), np.array(y_pred).astype(np.float16)
+
+fbeta_keras = common.f2_score(K.variable(y_true), K.variable(y_pred)).eval(session=K.get_session())
+fbeta_sklearn = common.f2_score_alt(y_true, y_pred)
+
+print('Scores are {:.3f} (sklearn) and {:.3f} (keras)'.format(fbeta_sklearn, fbeta_keras))
