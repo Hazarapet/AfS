190e404e742e4e5b74f168402879b26aa4017a8d
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-05-18 03:20:08 +0400

    batch size

--- exp2.py
+++ exp2.py
@@ -4,11 +4,4 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-y = [ [0, 1, 2, 3, 4], [5, 6, 7, 8, 19], [9, 8, 7, 6, 5] ]
-labels = ['foo', 'bar', 'baz']
-
-for y_arr, label in zip(y, labels):
-    plt.plot(y_arr, label=label)
-
-plt.legend(bbox_to_anchor=(1, 1), loc=5)
-plt.show()
+print [True, True, False, True, False] * 1
