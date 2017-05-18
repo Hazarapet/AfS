eb1839759224992ed4de4ec8b946566a32d4bec1
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-05-19 01:08:43 +0400

    losses test

--- exp.py
+++ exp.py
@@ -1,22 +1,24 @@
 import keras
 import numpy as np
 import keras.backend as K
+from keras import losses
 from utils import components
 from keras.models import Sequential
 from keras.layers import Dense
 
 
 model = Sequential()
-model.add(Dense(11, activation='relu', input_dim=100))
+model.add(Dense(4, activation='relu', input_dim=100))
 model.add(Dense(3, activation='sigmoid'))
 
 model.compile(optimizer='adam',
-              loss=components.reg_binary_cross_entropy(0.2),
+              loss=losses.binary_crossentropy,
               metrics=['accuracy'])
 
 # Generate dummy data
 data = np.random.random((10, 100)).astype(np.int8)
 labels = np.zeros((10, 3)).astype(np.int8)
+
 for i in range(labels.shape[0]):
     r_int = np.random.randint(3)
     labels[i][r_int] = 1
