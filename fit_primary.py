b1810df61d15d6c19ce902630b645cb8aa9e54a8
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-05-21 23:52:34 +0400

    mini fixes

--- fit_primary.py
+++ fit_primary.py
@@ -13,7 +13,7 @@ from models.primary.model import model as primary_model
 
 st_time = time.time()
 N_EPOCH = 3
-BATCH_SIZE = 160
+BATCH_SIZE = 180
 IMAGE_WIDTH = 128
 IMAGE_HEIGH = 128
 
@@ -47,7 +47,7 @@ train, val = df_train.values[:index], df_train.values[index:]
 print 'model loading...'
 [model, structure] = primary_model()
 
-adam = Adam(lr=1e-3, decay=0.)
+adam = Adam(lr=3e-4, decay=0.)
 
 model.compile(loss=components.f2_binary_cross_entropy(),
               optimizer=adam,
@@ -226,9 +226,9 @@ for epoch in range(N_EPOCH):
                 json_string = model.to_json()
                 json.dump(json_string, outfile)
 
-    if epoch == 5:
+    if epoch == 7:
         lr = model.optimizer.lr.get_value()
-        model.optimizer.lr.set_value(3e-4)
+        model.optimizer.lr.set_value(1e-4)
 
     if epoch == 10:
         lr = model.optimizer.lr.get_value()
