6535222cfc6e7e63245582f68b4e52b0496a773c
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-05-22 04:38:10 +0400

    exp3

--- exp3.py
+++ exp3.py
@@ -17,10 +17,13 @@ df_train = pd.read_csv('train_v2.csv')
 label_map = {l: i for i, l in enumerate(GROUP)}
 inv_label_map = {i: l for l, i in label_map.items()}
 
-for f, tags in df_train.values[:10000]:
+for f, tags in df_train.values[:5000]:
+    targets = np.zeros(8)
     for t in tags.split(' '):
         if t in GROUP:
-            print t
+            targets[label_map[t]] = 1
         else:
-            print 'other tag'  # other tag
+            targets[7] = 1
+
+        print targets
 
