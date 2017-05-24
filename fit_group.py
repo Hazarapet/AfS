12692378aca7346335e9b5313e43457b368f4347
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-05-24 22:46:01 +0400

    group

--- fit_group.py
+++ fit_group.py
@@ -13,7 +13,7 @@ from models.group.model import model as group_model
 
 st_time = time.time()
 N_EPOCH = 20
-BATCH_SIZE = 180
+BATCH_SIZE = 150
 IMAGE_WIDTH = 128
 IMAGE_HEIGH = 128
 GROUP = ['artisinal_mine'
