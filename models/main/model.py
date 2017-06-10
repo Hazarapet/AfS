1293d7a4af6c740751c77e3b7e580a46d4f35f80
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-06-10 20:04:54 +0400

    new model for jpg

--- models/main/model.py
+++ models/main/model.py
@@ -6,78 +6,90 @@ from keras.layers.normalization import BatchNormalization
 from keras.regularizers import l2
 
 def model(weights_path=None):
-    # _regular_input = layers.Input(shape=(3, 128, 128), name='regular_input')
-    # _sobel_input = layers.Input(shape=(3, 128, 128), name='sobel_input')
-    # merged = layers.concatenate([_regular_input, _sobel_input], axis=1)
-
     _model = Sequential()
-    # _model.add(Model([_regular_input, _sobel_input], merged))
     _model.add(ZeroPadding2D((1, 1), input_shape=(3, 128, 128)))
     _model.add(Conv2D(64, (3, 3)))
-    _model.add(BatchNormalization())
+    _model.add(BatchNormalization(axis=1))
     _model.add(Activation('relu'))
 
     _model.add(ZeroPadding2D((1, 1)))
     _model.add(Conv2D(64, (3, 3)))
-    # _model.add(BatchNormalization())
+    _model.add(BatchNormalization(axis=1))
     _model.add(Activation('relu'))
 
     _model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
     _model.add(ZeroPadding2D((1, 1)))
     _model.add(Conv2D(128, (3, 3)))
-    # _model.add(BatchNormalization())
+    _model.add(BatchNormalization(axis=1))
     _model.add(Activation('relu'))
 
     _model.add(ZeroPadding2D((1, 1)))
     _model.add(Conv2D(128, (3, 3)))
-    # _model.add(BatchNormalization())
+    _model.add(BatchNormalization(axis=1))
     _model.add(Activation('relu'))
 
     _model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
     _model.add(ZeroPadding2D((1, 1)))
     _model.add(Conv2D(128, (3, 3)))
-    # _model.add(BatchNormalization())
+    _model.add(BatchNormalization(axis=1))
     _model.add(Activation('relu'))
 
     _model.add(ZeroPadding2D((1, 1)))
     _model.add(Conv2D(128, (3, 3)))
-    # _model.add(BatchNormalization())
+    _model.add(BatchNormalization(axis=1))
     _model.add(Activation('relu'))
 
     _model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
     _model.add(ZeroPadding2D((1, 1)))
     _model.add(Conv2D(256, (3, 3)))
-    # _model.add(BatchNormalization())
+    _model.add(BatchNormalization(axis=1))
     _model.add(Activation('relu'))
 
     _model.add(ZeroPadding2D((1, 1)))
     _model.add(Conv2D(256, (3, 3)))
-    # _model.add(BatchNormalization())
+    _model.add(BatchNormalization(axis=1))
     _model.add(Activation('relu'))
 
     _model.add(ZeroPadding2D((1, 1)))
     _model.add(Conv2D(256, (3, 3)))
-    # _model.add(BatchNormalization())
+    _model.add(BatchNormalization(axis=1))
+    _model.add(Activation('relu'))
+
+    _model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+    _model.add(ZeroPadding2D((1, 1)))
+    _model.add(Conv2D(256, (3, 3)))
+    _model.add(BatchNormalization(axis=1))
+    _model.add(Activation('relu'))
+
+    _model.add(ZeroPadding2D((1, 1)))
+    _model.add(Conv2D(256, (3, 3)))
+    _model.add(BatchNormalization(axis=1))
+    _model.add(Activation('relu'))
+
+    _model.add(ZeroPadding2D((1, 1)))
+    _model.add(Conv2D(256, (3, 3)))
+    _model.add(BatchNormalization(axis=1))
     _model.add(Activation('relu'))
 
     _model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
     _model.add(ZeroPadding2D((1, 1)))
     _model.add(Conv2D(512, (3, 3)))
-    # _model.add(BatchNormalization())
+    _model.add(BatchNormalization(axis=1))
     _model.add(Activation('relu'))
 
     _model.add(ZeroPadding2D((1, 1)))
     _model.add(Conv2D(512, (3, 3)))
-    # _model.add(BatchNormalization())
+    _model.add(BatchNormalization(axis=1))
     _model.add(Activation('relu'))
 
     _model.add(ZeroPadding2D((1, 1)))
     _model.add(Conv2D(512, (3, 3)))
-    # _model.add(BatchNormalization())
+    _model.add(BatchNormalization(axis=1))
     _model.add(Activation('relu'))
 
     _model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
@@ -85,17 +97,15 @@ def model(weights_path=None):
     # Dense layers
     _model.add(Flatten())
 
-    _model.add(Dense(512, kernel_regularizer=l2(1e-4)))
+    _model.add(Dense(512, kernel_regularizer=l2(1e-5)))
     _model.add(Activation('relu'))
-    _model.add(Dropout(0.5))
 
-    _model.add(Dense(256, kernel_regularizer=l2(1e-4)))
+    _model.add(Dense(256, kernel_regularizer=l2(1e-5)))
     _model.add(Activation('relu'))
-    _model.add(Dropout(0.5))
 
     _model.add(Dense(17, activation='sigmoid'))
 
     if weights_path:
         _model.load_weights(weights_path)
 
-    return [_model, 'models/A/structures/']
+    return [_model, 'models/main/structures/']
