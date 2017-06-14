a7b4df3f07c830dcb5c23a81a2f8e786f5476bea
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-06-15 01:00:54 +0400

    nm with jpg input

--- fit_nm.py
+++ fit_nm.py
@@ -13,7 +13,7 @@ from models.nm.model import model as nm_model
 
 st_time = time.time()
 N_EPOCH = 20
-BATCH_SIZE = 20
+BATCH_SIZE = 25
 IMAGE_WIDTH = 128
 IMAGE_HEIGHT = 128
 
@@ -49,7 +49,7 @@ print 'model loading...'
 
 print model.summary()
 
-adam = Adam(lr=1e-1, decay=1e-4)
+adam = Adam(lr=3e-2, decay=1e-4)
 
 model.compile(loss=components.f2_binary_cross_entropy(),
               optimizer=adam,
@@ -207,11 +207,11 @@ for epoch in range(N_EPOCH):
 
     if epoch == 7:
         lr = model.optimizer.lr.get_value()
-        model.optimizer.lr.set_value(3e-2)
+        model.optimizer.lr.set_value(1e-2)
 
     if epoch == 10:
         lr = model.optimizer.lr.get_value()
-        model.optimizer.lr.set_value(1e-2)
+        model.optimizer.lr.set_value(3e-3)
 
     t_loss_graph = np.append(t_loss_graph, [np.mean(t_loss_graph_ep)])
     t_acc_graph = np.append(t_acc_graph, [np.mean(t_acc_graph_ep)])
