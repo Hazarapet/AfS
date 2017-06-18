bfacf8cdf4924f8d5655deb08afa405e24343127
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-06-18 15:40:04 +0400

    30 batch size

--- exp3.py
+++ exp3.py
@@ -1,6 +1,10 @@
 import sys
 import numpy as np
 import pandas as pd
+import h5py
 
-print range(6)
+f = h5py.File("models/nm/structures/densenet121_weights_th.h5", "r")
+
+for k, v in zip(f.keys(), f.values()):
+    print v.items()
 
--- fit_nm.py
+++ fit_nm.py
@@ -14,7 +14,7 @@ from models.nm.densenet121 import densenet121_model
 
 st_time = time.time()
 N_EPOCH = 40
-BATCH_SIZE = 32
+BATCH_SIZE = 30
 IMAGE_WIDTH = 224
 IMAGE_HEIGHT = 224
 AUGMENT = True
