801e43489167ff747f22875977470c0b929f8c08
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-06-20 04:47:57 +0400

    scratch

--- fit_nm.py
+++ fit_nm.py
@@ -60,7 +60,7 @@ sgd = SGD(lr=1e-1, momentum=.9, decay=1e-4)
 
 # adam = Adam(lr=1e-2, decay=1e-4)
 
-model.compile(loss=components.f2_binary_cross_entropy(l=0.5),
+model.compile(loss=components.f2_binary_cross_entropy(l=0.2),
               optimizer=sgd,
               metrics=[common_util.f2_score, 'accuracy'])
 
