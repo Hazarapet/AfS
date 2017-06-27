1eb51f706d24c0b80b7333f6df6fe16860e4d37c
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-06-28 03:27:04 +0400

    ensamble

--- test.py
+++ test.py
@@ -22,7 +22,7 @@ count = 0
 print 'images loading...'
 X_test = os.listdir('resource/test-jpg')
 
-result = predict.result_single_jpg(X_test, 'resource/test-jpg/{}')
+result = predict.result_single_jpg(X_test[:10], 'resource/test-jpg/{}')
 # Heng CherKeng's example
 thres = [0.05, 0.17, 0.05, 0.25, 0.32, 0.06, 0.1, 0.27, 0.28, 0.21, 0.09, 0.18, 0.16, 0.03, 0.2, 0.13, 0.04]
 # my_thres = [0.32, 0.02, 0.24, 0.01, 0.94, 0.05, 0.03, 0.24, 0.24, 0.19, 0.81, 0.13, 0.52, 0.08, 0.01, 0.12, 0.31]
@@ -31,7 +31,7 @@ with open('best_f2_threshold.json') as data_file:
     my_thres = json.load(data_file)
     my_thres = list(my_thres)
 
-df_test = pd.DataFrame([[p.replace('.jpg', ''), p] for p in X_test])
+df_test = pd.DataFrame([[p.replace('.jpg', ''), p] for p in X_test[:10]])
 df_test.columns = ['image_name', 'tags']
 
 tags = []
