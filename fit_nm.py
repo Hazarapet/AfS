046667b71058424308b86aab25c14ca12a3d412b
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-07-01 03:00:22 +0400

    mini fixes

--- fit_nm.py
+++ fit_nm.py
@@ -14,7 +14,7 @@ from models.nm.densenet121 import densenet121_model
 
 st_time = time.time()
 N_EPOCH = 20
-BATCH_SIZE = 62
+BATCH_SIZE = 40
 IMAGE_WIDTH = 224
 IMAGE_HEIGHT = 224
 AUGMENT = True  # TODO somethings wrong with this.It also makes train slower
