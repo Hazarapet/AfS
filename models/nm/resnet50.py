a0aa66eb1ac06ff2a3623524c340e2198efdcbd3
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-07-12 01:15:54 +0400

    mini fixes

--- models/nm/resnet50.py
+++ models/nm/resnet50.py
@@ -14,7 +14,7 @@ def model(weights_path=None):
 
     # 175 layers
     for i, layer in enumerate(_m.layers):
-        if i > 150:
+        if i > 160:
             break
 
         layer.trainable = False
@@ -22,15 +22,15 @@ def model(weights_path=None):
     x = _m.output
     x = Flatten(name='my_flatten_1')(x)
 
-    x = Dense(1024, kernel_regularizer=l2(2e-4), name='my_dense_1')(x)
+    x = Dense(512, kernel_regularizer=l2(2e-5), name='my_dense_1')(x)
     x = BatchNormalization(name='my_bn_1')(x)
     x = Activation('relu', name='my_act_1')(x)
-    x = Dropout(0.4, name='my_dp_1')(x)
+    x = Dropout(0.5, name='my_dp_1')(x)
 
-    x = Dense(512, kernel_regularizer=l2(1e-4), name='my_dense_2')(x)
+    x = Dense(512, kernel_regularizer=l2(1e-5), name='my_dense_2')(x)
     x = BatchNormalization(name='my_bn_2')(x)
     x = Activation('relu', name='my_act_2')(x)
-    x = Dropout(0.3, name='my_dp_2')(x)
+    x = Dropout(0.5, name='my_dp_2')(x)
 
     x = Dense(17, name='my_output_dense')(x)
     x = Activation('sigmoid', name='my_output')(x)
