42910571ff861c8d6da1b36a3c732ff6c157785b
Hazarapet Tunanyan <hazarapettunanyan@gmail.com>
2017-07-18 02:49:14 +0400

    152

--- models/nm/resnet50.py
+++ models/nm/resnet50.py
@@ -14,7 +14,7 @@ def model(weights_path=None):
 
     # 175 layers
     for i, layer in enumerate(_m.layers):
-        if i > 145:
+        if i > 152:
             break
 
         layer.trainable = False
